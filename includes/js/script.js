af3c515aa7813218d6471ea7cdbf5e4e628fb584
Paul Marceau <76534805+pmarceaujr@users.noreply.github.com>
2021-04-06 21:47:22 -0400

    Added code to toggle to other dates

--- includes/js/script.js
+++ includes/js/script.js
@@ -1,29 +1,49 @@
 //alert("Test")
 //Declare variables
 let dailyHours = ["12:00 AM", "1:00 AM", "2:00 AM", "3:00 AM", "4:00 AM", "5:00 AM", "6:00 AM", "7:00 AM", "8:00 AM", "9:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM", "5:00 PM", "6:00 PM", "7:00 PM", "8:00 PM", "9:00 PM", "10:00 PM", "11:00 PM"]
-let plannerTable = $("#plannerTable");
-//let plannerBody = $("#plannerTBody")
-let earlyMornTable = $("#earlyMornTable");
-//let earlyMornBody = $("#earlyMornBody")
-let lateAfterTable = $("#lateAfterTable");
-//let lateAfterBody = $("#lateAfterBody")
-let plannerContent = $('#plannerContent')
-//let plannerRow1 = $("<tr>")
-//console.log(plannerRow1)
-let plannerRowContent = "";
-//let
+let plannerTable = $("#plannerTable");                  //The section for the normal business hours events  
+let earlyMornTable = $("#earlyMornTable");              //The section for the early morning events   
+let lateAfterTable = $("#lateAfterTable");              //The section for the late afternoon events
+let plannerContent = $('#plannerContent');              //THe content div where the evnt items will display
+let minusOneDay = $("#minusOneDay");                    //Button for backing up one day  
+let plusOneDay = $("#addOneDay");                       //Button for advancing one day
+let plannerRowContent = "";                             // used to store the event description text
+let dailyEvents = {};                                   //object to store event data (time and description)
+let eventDate = dayjs().format('ddd, MMM DD, YYYY')     //the event date that is displayed in the JumboTron
+
 //Get the current date and load it into the jumboTron
 let plannerDate = $('#currentDay');
-plannerDate.text(dayjs().format('ddd, MMM DD, YYYY'))
+plannerDate.text(eventDate)
 plannerDate.append()
 
-//alert(dayjs('01/01/2000 13:15:22'))
+//Code to save user input when any of the save buttons are clicked.
+plannerTable.on('click', '.saveBtn', function () {
+    saveEventDesc()
+});
 
-for (let i = 0; i <= 23; i++) {
+//Code to back the calendar up one day.
+minusOneDay.on('click', function () {
+    eventDate = dayjs(eventDate).subtract(1, 'day').format('ddd, MMM DD, YYYY')
+    plannerDate.text(eventDate)
+    plannerDate.append()
+    loadEventDesc()
+});
+
+//Code to advance the calendar one day.
+plusOneDay.on('click', function () {
+    eventDate = dayjs(eventDate).add(1, 'day').format('ddd, MMM DD, YYYY')
+    plannerDate.text(eventDate)
+    plannerDate.append()
+    loadEventDesc()
+});
+
+//Build the 3 tables for the time slots in 1 hour increments, display hour, Event Text and a Save button.
+// table for early morning hours, normal business hours and late evening hours
+for (let i = 0; i <= dailyHours.length; i++) {
     plannerRowContent = (`<tr id='rowDesc${[i]}' class='row p-3'>`);
-    plannerRowContent += (`<td id='rowHour${[i]}' class='hour' >${dailyHours[i]}</td>`);
-    plannerRowContent += (`<td id='rowEvent${[i]}' class='description'><textarea  placeholder='Enter your event text here' cols=50></textarea></td>`);
-    plannerRowContent += (`<td id='rowSave${[i]}' class='saveBtn'>Save</td>`);
+    plannerRowContent += (`<td id='rowHour${[i]}' style="width: 15%" class='hour' >${dailyHours[i]}</td>`);
+    plannerRowContent += (`<td id='rowEvent${[i]}' style="width: 70%" class='description'><textarea  placeholder='Enter your event text here' cols=50></textarea></td>`);
+    plannerRowContent += (`<td id='rowSave${[i]}' style="width: 15%" class='btn saveBtn'>Save</td>`);
     plannerRowContent += ("</tr>");
     if (i < 8) {
         earlyMornTable.append(plannerRowContent)
@@ -36,18 +56,14 @@ for (let i = 0; i <= 23; i++) {
     }
 }
 
+//Format the background color of the event text area based on past, present or future.
 let plannerRow = plannerContent.find('tr')
 let plannerEvent = $('td[class*="desc"]')
-console.log(`length: ${plannerEvent.length}`)
-console.log(`event: ${plannerEvent}`)
 for (let i = 0; i < plannerEvent.length; i++) {
-    //console.log(plannerRow.find(`#rowEvent${i}`))
-    // let rowEventId = plannerRow.find(`#rowEvent${i}`).id
-    // console.log(`rowevent: ${rowEvent}`)
-    if (i === (parseInt(dayjs('01/01/2000 10:15:22').format('H')))) {
+    if (i === (parseInt(dayjs().format('H')))) {
         $(`#${plannerEvent[i].id}`).addClass("present")
     }
-    else if (i < (parseInt(dayjs('01/01/2000 10:15:22').format('H')))) {
+    else if (i < (parseInt(dayjs().format('H')))) {
         $(`#${plannerEvent[i].id}`).addClass("past")
     }
     else {
@@ -55,41 +71,31 @@ for (let i = 0; i < plannerEvent.length; i++) {
     }
 }
 
-
-//alert("tb end")
-//$("#dailyRows").append("</tbody>");
-//$("#dailyRows").append("</table>");
-/*
-      <section id="hours" class="hour col-12 col-md-1">hours</section>
-      <section id="events" class="description col-12 col-md-10">events</section>
-      <section id="delete" class="saveBtn col-12 col-md-1">X</section>
-
-        <tr class="d-flex">
-          <td class="hour col-12 col-md-1">hours</td>
-          <td class="description col-12 col-md-10">
-            <textarea name="" id="">This is my event</textarea>
-          </td>
-          <td class="saveBtn col-12 col-md-1">Save</td>
-        </tr>
-
-function createTable() {
-    //alert("test")
-    let projectTable = $("#theTable")
-    let projectTableTh = $('<thead class="thead-dark" ><td>Project Name</td><td>Project Type</td><td>Hourly wage</td><td>Due date</td><td>Days until</td><td>Estimated total</td><td>Delete</td></thead>')
-    //projectTable.text(');
-    projectTable.append(projectTableTh);
-    var content = "<table>"
-    for (i = 0; i < 3; i++) {
-        content += `< tr ><td> row ${i} column 1</td> <td> row ${i} columnsds 1</td> <td> row ${i} column 1</td> <td> row ${i} column 1</td> <td> row ${i} column 1</td></tr > `;
+//The save daily events description function will save the user input to local storage
+function saveEventDesc() {
+    for (let i = 0; i <= dailyHours.length; i++) {
+        eventDescText = $(`#rowEvent${[i]} textarea`).val()
+        evntDescProp = `rowEvent${[i]}`
+        dailyEvents[evntDescProp] = eventDescText
+        localStorage.setItem(plannerDate.text(), JSON.stringify(dailyEvents));
     }
-    content += "</table>"
-
-    $('#theTable').append(content);
-
-
-    //projectTable.append(projectTableTh);
+    alert("Changes have been saved.")
 }
-createTable()
 
-
-*/
+//function to retrieve the saved event description, that is stored in local storage
+function loadEventDesc() {
+    // alert(plannerDate.text())
+    dailyEvents = {}
+    dailyEvents = JSON.parse(localStorage.getItem(plannerDate.text()));
+    console.log(dailyEvents)
+    if (dailyEvents) {
+        for (let key in dailyEvents) {
+            $(`#${key} textarea`).val(dailyEvents[key])
+        }
+    }
+    else {
+        alert("need to reset the event date")
+    }
+}
+//Invoke the loadEventDesc function to load the event desc text into their respective rows in the
+loadEventDesc()
